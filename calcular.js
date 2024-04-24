86f3482f063dc5518447e2d5ec27a65c3b3f615a
Flaviana <104791933+flavianaal@users.noreply.github.com>
2024-04-24 10:50:51 -0300

    Create calcular.js
    Calcular Nivel de Vitorias

---
+++ calcular.js
@@ -0,0 +1,32 @@
+function calcularNivel(vitorias, derrotas) {
+  
+    const saldoVitorias = vitorias - derrotas;
+
+  
+    let nivel;
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+  
+    return {
+        saldoVitorias: saldoVitorias,
+        nivel: nivel
+    };
+}
+
+
+const resultado = calcularNivel(30, 15);
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível de ${resultado.nivel}`);
